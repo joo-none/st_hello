87132c098d1dd07bda765231be2f49602fcee6f0
Jun Eon Lee <126752298+joo-none@users.noreply.github.com>
2023-05-03 17:43:42 +0900

    Update 종목토론방.py

--- 종목토론방.py
+++ 종목토론방.py
@@ -45,10 +45,43 @@ def jongmok_context(item_code, page_no):
     context_url_list = [base_url + i for i in sub_url_list]
     return context_url_list
 
+# 원하는 페이지만큼 내용 링크를 수집하는 함수 만들기
+def context_link(item_code, last_page):
+    b=[]
+    for i in range(1, last_page + 1):
+        a = jongmok_context(item_code, i)
+        b.append(a)
+        context_list = pd.concat([pd.DataFrame(x) for x in b], ignore_index=True)
+    return context_list
+
+# 종목토론 목록과 내용링크를 포함한 데이터프레임 만들기
+def 목록_내용링크(item_code, last_page):
+    종목토론_목록 = jongmok_toron(item_code, last_page)
+    종목토론_내용링크 = context_link(item_code, last_page)
+    a = pd.concat([종목토론_목록, 종목토론_내용링크], axis=1, ignore_index=True)
+    a.columns = ['날짜', '제목', '글쓴이', '조회', '공감', '비공감', '내용링크']
+    return a
+
+# 내용을 수집하는 함수
+def 종목토론_내용(item_code, last_page):
+    content = []
+    df = 목록_내용링크(item_code, last_page)
+    for i in df['내용링크']:
+        headers = {"user-agent": "Mozilla/5.0"}
+        response = requests.get(i, headers=headers)
+        html = BeautifulSoup(response.text)
+        a = html.select('#body')[0].text
+        content.append(a)        
+    return content
+
+def 종목토론_목록_내용링크_내용(item_code, last_page):
+    df = 목록_내용링크(item_code, last_page)
+    df['내용'] = 종목토론_내용(item_code, last_page)
+    return df
 
 item_code = '086520'
-page_no = 1
-st.dataframe(jongmok_context(item_code, page_no))
+last_page = 1
+st.dataframe(종목토론_목록_내용링크_내용(item_code, last_page))
 
 
 
